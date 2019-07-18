d719768c4afafed553c09044099cb3b66934390d
barnswills <barneywilliams90@hotmail.co.uk>
2019-07-18 16:50:19 +0100

    commit 18/07 #1

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+    <h1>Advanced Component</h1>
+    <app-quote quote="a better quote">
+      <h2>{{quoteTitle}}</h2>
+      <h2>Quote</h2>
+    </app-quote>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Quote from "./components/Quote";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+
+  data: () => {
+    return { quoteTitle: "The Quote"}
+  },
+  components: { appQuote: Quote }
 };
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +30,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  h2 {
+    color: red;
+  }
 </style>
